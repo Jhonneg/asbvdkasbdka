33dc9ccfb8c2c42a8f6152f9f5c0d0806a6d1e22
Jonee2 <94491571+Jonee2@users.noreply.github.com>
2024-08-18 20:00:18 -0300

    added tailwind config

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,11 @@
-import "./App.css";
 import ButtonGradient from "./assets/svg/ButtonGradient";
-import Button from "./components/Button";
+import Header from "./components/Header";
 
 export default function App() {
   return (
     <main>
-      <div>Hello</div>
       <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
-        <Button className="mt-10">
-          Something
-        </Button>
+        <Header />
       </div>
       <ButtonGradient />
     </main>
