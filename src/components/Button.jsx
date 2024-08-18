90983a17118f02fc628ddf32bb215d6f8ad8e84f
Jonee2 <94491571+Jonee2@users.noreply.github.com>
2024-08-18 20:45:32 -0300

    initial header styling

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,13 @@
 import ButtonSvg from "../assets/svg/ButtonSvg";
 
-const Button = ({ classname, href, onClick, children, px, white }) => {
+export default function Button({
+  classname,
+  href,
+  onClick,
+  children,
+  px,
+  white,
+}) {
   const classes = `button relative inline-flex items-center justify-center h-11 transition-colors hover:text-color-1 ${
     px || "px-7"
   } ${white ? "text-n-8" : "text-n-1"} ${classname || ""}`;
@@ -19,6 +26,4 @@ const Button = ({ classname, href, onClick, children, px, white }) => {
     </a>
   );
   return href ? renderLink() : renderButton();
-};
-
-export default Button;
+}
