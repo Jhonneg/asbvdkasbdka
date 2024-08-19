dcaac5b00298208c713c564e4cdc3ec4e5b68181
Jonee2 <94491571+Jonee2@users.noreply.github.com>
2024-08-19 01:39:47 -0300

    fixed section

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -8,9 +8,10 @@ export default function Button({
   px,
   white,
 }) {
-  const classes = `button relative inline-flex items-center justify-center h-11 transition-colors hover:text-color-1 ${
-    px || "px-7"
-  } ${white ? "text-n-8" : "text-n-1"} ${classname || ""}`;
+  const classes = `button relative inline-flex items-center 
+  justify-center h-11 transition-colors hover:text-color-1 ${px || "px-7"} ${
+    white ? "text-n-8" : "text-n-1"
+  } ${classname || ""}`;
 
   const spanClasses = "relative z-10";
 
